298f568626c7d40223be9571f512533f995f5067
AGCTanaka <bluejb.bluejay@iloud.com>
2019-07-09 13:39:07 +0900

    0709

--- hello.py
+++ hello.py
@@ -1,17 +1,20 @@
 # -*- coding:utf-8 -*-
 # Flask などの必要なライブラリをインポートする
-from flask import Flask
+from flask import Flask,render_template
 import os
 import numpy as np
 
 # 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 
-# ここからウェブアプリケーション用のルーティングを記述
-# index にアクセスしたときの処理
+#アプリケーションルートにアクセスがあった場合
 @app.route('/')
-def index():
+def hello():
     return "Hello,World!"
 
+#アプリケーション/indexにアクセスがあった場合
+@app.route("/index")
+def index():
+    return render_template("index.html")
 if __name__ == '__main__':
     app.run() # どこからでもアクセス可能に
