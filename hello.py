415b945ddd98b39f9739d337515795a7029bdc18
AGCTanaka <bluejb.bluejay@iloud.com>
2019-07-10 14:52:10 +0900

    test

--- hello.py
+++ hello.py
@@ -1,27 +1,64 @@
 # -*- coding:utf-8 -*-
 # Flask などの必要なライブラリをインポートする
 from flask import Flask,render_template,request,redirect,url_for
+import requests
+import json
+import re
 import os
 import numpy as np
+
+#きょうのトークンせってい
+token = "JxvI5GqPJucJfthIFcjHxdbBHc06"
 
 # 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 
 def error_detect(text):
-    pass
-    #Cotoha APIサーバーにルーティングする？
-    #いつも通りリクエストを投げつける
-    #戻り値を得てリターン
+    url = "https://api.ce-cotoha.com/api/dev/nlp/beta/detect_misrecognition"
+    headers = {
+            "Content-Type":"application/json;charset=UTF-8",
+            "Authorization":"Bearer " + token
+            }
+    data = {
+            "sentence":text
+            }
+    r_post = requests.post(url,headers=headers,json=data)
+    r_json=r_post.json()
+    return r_json
 
-def vm():
-    pass
-    #ボタンが押されたらerror_detectを動かす
+def make_sentence(text,json):
+#begin_posを取得
+#end_posを取得
+#textのbegin_posからend_posまでをcorrection[0]に置き換える
+    jstext = json["result"]
+    if jstext["candidates"] == []:
+        text = text + "\n" + "<認識誤りは検知されませんでした。>"
+        return text
+    else:
+        for adjust in jstext["candidates"]:
+            beginpos = adjust["begin_pos"]
+            endpos = adjust["end_pos"]
+            head = text[:beginpos]
+            berry = adjust["correction"][0]
+            tail = text[endpos:]
+            text = head + berry["form"] + tail
+        return text
+
 
 #アプリケーションルートにアクセスがあった場合
 @app.route('/')
 def hello():
     #vm()を動かす
-    return render_template("index.html",title = "ErrorDetectTest")
+    return render_template("index.html",title = "ErrorDetectTest",name1 = "修正したい音声ファイルを入力してください",name2 = "修正後のテキストがここに表示されます")
+
+@app.route('/post',methods=['GET','POST'])
+def post():
+    title = "layout2"
+    if request.method == 'POST':
+        text = request.form['text']
+        detectedText = error_detect(text)
+        writeText = make_sentence(text,detectedText)
+    return render_template('index.html',name1=text,name2=writeText,title = title)
+
 if __name__ == '__main__':
     app.run() # どこからでもアクセス可能に
-
---
+++ test.py
@@ -0,0 +1,1 @@
+json = "{'result':{'score':0.99,'candidates':[{'begin_pos':0,'end_pos':2,'form':'温泉','correction':[{'form':'音声','correct_score':0.99}]}]}},'status':0,'message':OK"
