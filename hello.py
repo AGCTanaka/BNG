ef971895afb17884895fb4c73565c2dc865bd5c9
AGCTanaka <bluejb.bluejay@iloud.com>
2019-07-11 17:10:25 +0900

    test

--- hello.py
+++ hello.py
@@ -13,6 +13,10 @@ token = "GdlHdDTsGCogk5IqSMXZaXbec5Aa"
 # 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
 
+#以下各APIを起動するための関数を定義する
+
+
+#COTOHA API 音声認識誤り検知
 def error_detect(text):
     url = "https://api.ce-cotoha.com/api/dev/nlp/beta/detect_misrecognition"
     headers = {
@@ -45,18 +49,39 @@ def make_sentence(text,json):
         return text
 
 
+
+#COTOHA API 感情分析
+def detect_sentiment(text,json):
+#begin_posを取得
+#end_posを取得
+#textのbegin_posからend_posまでをcorrection[0]に置き換える
+    jstext = json["result"]
+    if jstext["emotional_phrase"] == []:
+        text = text + "\n" + "<感情値は検知されませんでした。>"
+        return text
+    else:
+        retvar = ""
+        for emotions in jstext["emotional_phrase"]:
+            root =emotions["form"]
+            emotion =emotions["emotion"]
+            retvar = retvar + "\n" + emotion + "\n" + root
+        return retvar
+
+
+
 #アプリケーションルートにアクセスがあった場合
 @app.route('/')
 def hello():
-    return render_template("index.html",title = "ErrorDetectTest",name1 = "温泉認識は誤りを含む。",name2 = "")
+    return render_template("index.html",title = "COTOHA API test",name1 = "温泉認識は誤りを含む。",name2 = "")
 
+#プルダウンの値を読み取り、内容によって用いる関数と使い方部分に渡すパラグラフを変更する。
 @app.route('/post',methods=['GET','POST'])
 def post():
     if request.method == 'POST':
         text = request.form['text']
         detectedText = error_detect(text)
         writeText = make_sentence(text,detectedText)
-    return render_template('index.html',name1=text,name2=writeText,title ="ErrorDetectTestResult")
+    return render_template('index.html',name1=text,name2=writeText,title ="API Test Result")
 
 if __name__ == '__main__':
     app.run() # どこからでもアクセス可能に
