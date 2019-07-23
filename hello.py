848f0e653f56c0d1d34147b5eae66d67a9fdbaab
AGCTanaka <bluejb.bluejay@iloud.com>
2019-07-23 13:51:19 +0900

    test

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 def ip_check(func):
     def wrapper(*args,**kwargs):
-        if re.search(request.remote_addr,"10\..*"):
+        if re.match(request.remote_addr,"\A10\..*"):
             print ("IP Check : Okay")
             return func(*args,**kwargs)
         else:
